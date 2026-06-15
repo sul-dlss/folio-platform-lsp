b462bf1a297a253a8ab5cbd8d509ba5af15753db
Shelley Doljack <sdoljack@stanford.edu>
2026-06-15 14:28:18 -0600

    Customizes R1-2025-csp-7 for folio-test.

--- stripes.config.js
+++ stripes.config.js
@@ -3,32 +3,37 @@ const modules = require("./stripes.modules");
 module.exports = {
   okapi: {
     // application gateway
-    'url': '${kongUrl}',
-    'uiUrl': '${tenantUrl}',
+    'url': 'https://folio-test-api.stanford.edu',
+    'uiUrl': 'https://folio-test.stanford.edu',
     // authentication details: url, secret, clientId
-    'authnUrl': '${keycloakUrl}',
+    'authnUrl': 'https://keycloak-folio-test.stanford.edu',
   },
   config: {
     isEureka: true,
-    hasAllPerms: ${hasAllPerms},
+    hasAllPerms: false,
+    welcomeMessage: 'FOLIO TEST Sunflower CSP 7 - Stanford University',
+    platformName: 'FOLIO TEST Sunflower CSP 7',
+    helpUrl: 'https://sites.google.com/stanford.edu/folio-training-central/help',
     logCategories: 'core,path,action,xhr',
     useSecureTokens: true,
     idleSessionWarningSeconds: 60,
     logPrefix: '--',
     maxUnpagedResourceCount: 2000,
     showPerms: false,
-    aboutInstallDate: ${aboutInstallDate},
-    aboutInstallMessage: ${aboutInstallMsg},
-    tenantOptions: ${tenantOptions}
+    aboutInstallDate: '2026-06-15',
+    aboutInstallMessage: 'Eureka Test Sunflower CSP 7',
+    enableEcsRequests: false,
+    isSingleTenant: true,
+    tenantOptions: {sul: {name: "sul", clientId: "sul-application"}}
   },
   modules, // Populated by stripes.modules.js
   branding: {
     logo: {
-      src: './logo.png',
-      alt: '${tenant_name}'
+      src: './tenant-assets/logo.png',
+      alt: 'Stanford University'
     },
     favicon: {
-      src: './favicon.png'
+      src: './tenant-assets/stanford-favicon.png'
     },
   }
 };
--- stripes.modules.js
+++ stripes.modules.js
@@ -27,10 +27,8 @@ module.exports = {
   '@folio/lists': {},
   '@folio/local-kb-admin': {},
   '@folio/marc-authorities': {},
-  "@folio/mosaic-settings": {},
   '@folio/myprofile' : {},
   '@folio/notes' : {},
-  '@folio/oai-pmh' : {},
   '@folio/orders': {},
   '@folio/organizations' : {},
   '@folio/serials-management' : {},
@@ -52,6 +50,7 @@ module.exports = {
   '@folio/plugin-find-po-line': {},
   '@folio/plugin-find-user' : {},
   '@folio/plugin-query-builder': {},
+  '@folio/plugin-select-application' : {},
   '@folio/quick-marc': {},
   '@folio/receiving' : {},
   '@folio/remote-storage' : {},
@@ -64,6 +63,4 @@ module.exports = {
   '@folio/tags': {},
   '@folio/tenant-settings' : {},
   '@folio/users' : {},
-  '@folio/plugin-select-application' : {},
-  '@folio/lists': {}
 };
