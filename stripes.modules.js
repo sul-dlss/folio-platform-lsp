70956f67459f3905a72a664bec9fe205ea0b3a9a
Shelley Doljack <sdoljack@stanford.edu>
2026-07-16 07:29:54 -0600

    Adds reading-room app to stripes modules.

--- stripes.modules.js
+++ stripes.modules.js
@@ -54,6 +54,7 @@ module.exports = {
   '@folio/plugin-query-builder': {},
   '@folio/plugin-select-application' : {},
   '@folio/quick-marc': {},
+  '@folio/reading-room': {},
   '@folio/receiving' : {},
   '@folio/remote-storage' : {},
   '@folio/requests' : {},
